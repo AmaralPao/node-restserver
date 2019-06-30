0797c089ecc04331d653487b088e1e3904e03abf
pao <amaralcastillon@gmail.com>
2019-06-30 02:17:31 -0500

    dd

---
+++ server/server.js
@@ -0,0 +1,49 @@
+require('./config/config')
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+ 
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', function (req, res) {
+  res.send('Hello World');
+});
+
+app.get('/usuario',(req,res)=>{
+    res.send('get usuario');
+
+});
+ 
+app.post('/usuario',(req,res)=>{
+    let body=req.body;
+    if(body.nombre === undefined){
+        res.status(400).json({
+            ok:false,
+            mensaje:'El nombre es necesario'
+        });
+    }else{
+        res.json({
+           persona:body 
+        });
+
+    }
+
+});
+app.put('/usuario/:id',(req,res)=>{
+    let id=req.params.id;
+    res.json({
+        id
+    
+    });
+
+});
+app.delete('/usuario',(req,res)=>{
+    res.send('delete usuario');
+
+});
+app.listen(process.env.PORT,()=>{
+    console.log('escuchando puerto',process.env.PORT);
+});
